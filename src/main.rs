a4195fa6fff8eea515a7d9b0bdb4075ec0e86945
mustafademiray <mustafademiray@protonmail.com>
2023-10-06 23:33:44 +0300

    database implementations

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use actix_cors::Cors;
-use actix_web::{http::header, web, App, HttpServer, Responder, HttpResponse};
-use serde::{Deserialize, Serialize};
+use actix_web::{ http::header, web, App, HttpServer, Responder, HttpResponse };
+use serde::{ Deserialize, Serialize };
 use reqwest::Client as HttpClient;
 use async_trait::async_trait;
 
@@ -9,12 +9,80 @@ use std::collections::HashMap;
 use std::fs;
 use std::io::Write;
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Task {
+    id: u64,
+    name: String,
+    completed: bool
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct User {
+    id: u64,
+    name: String,
+    password: String
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Database {
+    tasks: HashMap<u64, Task>,
+    users: HashMap<u64, User>
+}
 
 
+impl Database {
+    fn new() -> Self {
+        Self {
+            tasks: HashMap::new(),
+            users: HashMap::new()
+        }
+    }
 
+    //CRUD
+    fn insert(&mut self, task: Task) {
+        self.tasks.insert(task.id, task);
+    }
 
+    fn get(&self, id: &u64) -> Option<&Task> {
+        self.tasks.get(id)        
+    }
 
+    fn get_all(&self) -> Vec<&Task>{
+        self.tasks.values().collect()
+    }
 
+    fn delete(&mut self, id: &u64) {
+        self.tasks.remove(id);
+    }
+
+    fn update(&mut self, task: Task) {
+        self.tasks.insert(task.id, task);
+    } 
+
+    //user related functions
+    fn insert_user(&mut self, user: User) {
+        self.users.insert(user.id, user);
+    }
+
+    fn get_user_by_name(&self, username: &str) -> Option<&User> {
+        self.users.values().find(|u| u.name == username)
+    }
+
+    //database saving
+    fn save_to_file(&self) -> std::io::Result<()> {
+        let data = serde_json::to_string(&self)?;
+        let mut file = fs::File::create("database.json")?;
+        file.write_all(data.as_bytes())?;
+        Ok(())
+    }
+
+    fn load_from_file() -> std::io::Result<Self> {
+        let file_content = fs::read_to_string("database.json")?;
+        let db: Database = serde_json::from_str(&file_content)?;
+        Ok(db)
+    }
+
+}
 
 fn main() {
     println!("Hello, world!");
