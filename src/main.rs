8cd2dc3e4d8649d67d254e81c918aec1d6cd5122
mustafademiray <mustafademiray@protonmail.com>
2023-10-07 02:15:42 +0300

    mock db connection

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,39 @@
 use actix_cors::Cors;
-use actix_web::{ http::header, web, App, HttpServer, Responder, HttpResponse };
-use serde::{ Deserialize, Serialize };
-use reqwest::Client as HttpClient;
+use actix_web::{http::header, web, App, HttpResponse, HttpServer, Responder};
 use async_trait::async_trait;
+use reqwest::Client as HttpClient;
+use serde::{Deserialize, Serialize};
 
-use std::sync::Mutex;
 use std::collections::HashMap;
 use std::fs;
 use std::io::Write;
+use std::sync::Mutex;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 struct Task {
     id: u64,
     name: String,
-    completed: bool
+    completed: bool,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 struct User {
     id: u64,
     name: String,
-    password: String
+    password: String,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 struct Database {
     tasks: HashMap<u64, Task>,
-    users: HashMap<u64, User>
+    users: HashMap<u64, User>,
 }
-
 
 impl Database {
     fn new() -> Self {
         Self {
             tasks: HashMap::new(),
-            users: HashMap::new()
+            users: HashMap::new(),
         }
     }
 
@@ -44,10 +43,10 @@ impl Database {
     }
 
     fn get(&self, id: &u64) -> Option<&Task> {
-        self.tasks.get(id)        
+        self.tasks.get(id)
     }
 
-    fn get_all(&self) -> Vec<&Task>{
+    fn get_all(&self) -> Vec<&Task> {
         self.tasks.values().collect()
     }
 
@@ -57,7 +56,7 @@ impl Database {
 
     fn update(&mut self, task: Task) {
         self.tasks.insert(task.id, task);
-    } 
+    }
 
     //user related functions
     fn insert_user(&mut self, user: User) {
@@ -81,13 +80,10 @@ impl Database {
         let db: Database = serde_json::from_str(&file_content)?;
         Ok(db)
     }
-
-
 }
 
-
 struct AppState {
-    db: Mutex<Database>
+    db: Mutex<Database>,
 }
 
 async fn create_task(app_state: web::Data<AppState>, task: web::Json<Task>) -> impl Responder {
@@ -97,29 +93,26 @@ async fn create_task(app_state: web::Data<AppState>, task: web::Json<Task>) -> i
     HttpResponse::Ok().finish()
 }
 
-
 #[actix_web::main]
-async fn main () -> std::io::Result<()> { 
+async fn main() -> std::io::Result<()> {
     let db = match Database::load_from_file() {
         Ok(db) => db,
-        Err(_) => Database::new()
+        Err(_) => Database::new(),
     };
-    let data = web::Data::new(AppState{
-        db: Mutex::new(db)
-    });
+    let data = web::Data::new(AppState { db: Mutex::new(db) });
 
     HttpServer::new(move || {
         App::new()
             .wrap(
                 Cors::permissive()
-                    .allowed_origin_fn(|origin, _req_head|{
+                    .allowed_origin_fn(|origin, _req_head| {
                         origin.as_bytes().starts_with(b"http://localhost") || origin == "null"
-                    })   
-                    .allowed_methods(vec!["GET","POST", "PUT", "DELETE"])
+                    })
+                    .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
                     .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
                     .allowed_header(header::CONTENT_TYPE)
                     .supports_credentials()
-                    .max_age(3600)
+                    .max_age(3600),
             )
             .app_data(data.clone())
             .route("/task", web::post().to(create_task))
